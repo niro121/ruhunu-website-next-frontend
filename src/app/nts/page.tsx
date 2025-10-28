49d2aaae1dac3a6cb51c0bb8af37a98a8dcdd8af
Ruchaka96 <ruchakalakshan1996@gmail.com>
2025-10-28 12:07:56 +0530

    add a nts page

---
+++ src/app/nts/page.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import Banner from "@/components/global/banner";
+import Overview from "@/components/nts/overview";
+import VisionMission from "@/components/nts/visionmission";
+import NursingCollegeSection from "@/components/nts/nursingcollegesection";
+import NursingBanner from "@/components/nts/nursingbanner";
+import Gallery from "@/components/nts/gallery";
+import ContactCard from "@/components/nts/contactcard";
+
+export default function page() {
+  return (
+    <div>
+      <Banner
+        title="Nursing Training School (NTS)"
+        backgroundImage="/images/ntswebbg.png"
+        mobileBackgroundImage="/images/ntsmobilebg.jpg"
+        breadcrumbs={[{ label: "Home", href: "/" }, { label: "Nursing Training School" }]}
+      />
+
+      <Overview/>
+      <VisionMission />
+      <NursingCollegeSection />
+      <NursingBanner />
+      <Gallery />
+      <ContactCard />
+    </div>
+  );
+}
