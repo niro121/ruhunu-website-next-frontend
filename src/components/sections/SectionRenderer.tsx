a1d24798a9d0646ba2a917112d7b850aadeaad83
Chathuranga Dilshan <superdilshan999@gmail.com>
2025-12-03 10:08:50 +0530

    add all secting switch case

--- src/components/sections/SectionRenderer.tsx
+++ src/components/sections/SectionRenderer.tsx
@@ -9,13 +9,69 @@ export default function SectionRenderer({ section }: any) {
             return <HeroSlider layout={layout} data={data}/>;
             break;
 
-        case "Text Text":
+        case "Text Only":
             return <AboutUs/>;
             break;
 
-        // case "Accordion":
-        //   if (layout === 1) return <AccordionLayout1 data={data} />;
-        //   break;
+        case "Text-Text":
+            return <AboutUs/>;
+            break;
+
+        case "Text-Media":
+            return <AboutUs/>;
+            break;
+
+        case "Media Only":
+            return <AboutUs/>;
+            break;
+
+        case "Media-Media":
+            return <AboutUs/>;
+            break;
+
+        case "Extra Large Text":
+            return <AboutUs/>;
+            break;
+
+        case "Spacer":
+            return <AboutUs/>;
+            break;
+
+        case "Award":
+            return <AboutUs/>;
+            break;
+
+        case "Banner":
+            return <AboutUs/>;
+            break;
+        
+        case "Borad Of Directors":
+            return <AboutUs/>;
+            break;
+
+        case "Caousel":
+            return <AboutUs/>;
+            break;
+
+        case "Accordion":
+          return <></>;
+          break;
+
+        case "Documents":
+            return <AboutUs/>;
+            break;
+
+        case "Media":
+            return <AboutUs/>;
+            break;
+
+        case "PopUp":
+            return <AboutUs/>;
+            break;
+
+        case "Map":
+            return <AboutUs/>;
+            break;
 
         default:
             return (
