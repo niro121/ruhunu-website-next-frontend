bff0a5bfd1653d28105257e9b26f7f9c321a2432
Hiruna SP <hirunasp@gmail.com>
2025-12-11 13:24:27 +0530

    Convert the text-media-component to dynamic

--- src/components/cms/text-media-component.tsx
+++ src/components/cms/text-media-component.tsx
@@ -43,27 +43,123 @@
 import Image from "next/image";
 
 type TextMediaProps = {
-    data: any;
-}
+  data: any;
+  layout: number;
+};
 
-export default function TextMedia ({data}: TextMediaProps) {
-    console.log({data})
+export default function TextMedia({ data, layout }: TextMediaProps) {
+  console.log({ data });
+
+  let layoutValue = layout;
+  console.log({ layoutValue });
+  if (layoutValue === 1) {
     return (
-        <div className="px-[10px] md:px-[20px] lg:px-[50px] py-[70px] bg-white grid grid-cols-1 md:grid-cols-2 gap-[30px]">
-            <div className="relative w-full h-[480px]">
-                <Image
-                    src={data.image}
-                    alt="about us image"
-                    fill
-                    className=""
-                    priority
-                />
-            </div>
-            <div className="flex flex-col justify-center">
-                <p className="text-[#18CE67] mb-[16px] text-[16px]">About Us</p>
-                <h1 className="text-[40px] font-bold text-[#122739]">Leading the Way in Healthcare Excellence</h1>
-                <p className="text-black text-[16px] mt-[48px] mb-[16px]">Ruhunu Hospitals (Pvt) Ltd, located in the medical hub of the Southern Province, has emerged as a leading private hospital since its establishment in 1995. With a strong commitment to providing high-quality and convenient medical care, Ruhunu Hospitals has become the go-to healthcare provider in the region. Its continuous innovation and focus on delivering premium healthcare services have set it apart from others in the industry.</p>
-            </div>
+      <div className="px-[10px] md:px-[20px] lg:px-[50px] py-[70px] bg-white grid grid-cols-1 md:grid-cols-2 gap-[30px]">
+        <div className="sm:hidden relative w-full h-[480px]">
+          <Image
+            src={data.mobileImage || ""}
+            alt="about us image"
+            fill
+            className=""
+            priority
+          />
         </div>
-    )
+        <div className="hidden sm:block relative w-full h-[480px]">
+          <Image
+            src={data.webImage}
+            alt="about us image"
+            fill
+            className=""
+            priority
+          />
+        </div>
+        <div className="flex flex-col justify-center">
+          <p className="text-[#18CE67] mb-[16px] text-[16px]">{data.title}</p>
+          <h1 className="text-[40px] font-bold text-[#122739]">
+            {data.subTitle}
+          </h1>
+          <div
+            className="text-black text-[16px] mt-[48px] mb-[16px]"
+            dangerouslySetInnerHTML={{ __html: data.content }}
+          />
+        </div>
+      </div>
+    );
+  } else if (layoutValue === 2) {
+    return (
+      <div className="px-[10px] md:px-[20px] lg:px-[50px] py-[70px] bg-white grid grid-cols-1 md:grid-cols-2 gap-[30px]">
+        <div className="flex flex-col justify-center">
+          <p className="text-[#18CE67] mb-[16px] text-[16px]">{data.title}</p>
+          <h1 className="text-[40px] font-bold text-[#122739]">
+            {data.subTitle}
+          </h1>
+          <div
+            className="text-black text-[16px] mt-[48px] mb-[16px]"
+            dangerouslySetInnerHTML={{ __html: data.content }}
+          />
+        </div>
+
+        <div className="block md:hidden relative w-full h-[480px]">
+          <Image
+            src={data.mobileImage || ""} 
+            alt="about us image"
+            fill
+            className=""
+            priority
+          />
+        </div>
+        <div className="hidden md:block relative w-full h-[480px]">
+          <Image
+            src={data.webImage}
+            alt="about us image"
+            fill
+            className=""
+            priority
+          />
+        </div>
+      </div>
+    );
+  } else if (layoutValue >= 3) {
+    return (
+      <section
+        id="MessageSection"
+        className="w-full px-[10px] md:px-[20px] lg:px-[50px] pb-[70px] flex flex-col gap-10 text-[#122739] bg-[#f4f9fd]"
+      >
+        <div className="flex flex-col md:flex-row items-center md:items-start px-[12px]">
+          <div className="w-full md:w-5/7 text-center md:text-left mt-[40px] pl-[24px] pr-[12px]:">
+            <h3 className="text-4xl font-extrabold mb-2 ">{data.title}</h3>
+            <div
+              className="text-black text-[16px] mt-[48px] mb-[16px] leading-relaxed"
+              dangerouslySetInnerHTML={{ __html: data.content }}
+            />
+          </div>
+          <div className="w-full md:w-2/7 mt-6 md:mt-0 flex justify-center md:justify-end px-[12px]">
+            {/* Desktop Image */}
+            <div className="hidden md:block">
+              <Image
+                src={data.webImage}
+                alt={data.title}
+                width={300}
+                height={360}
+                className="object-cover shadow-md"
+                priority
+              />
+            </div>
+
+            {/* Mobile Image */}
+            <div className="block md:hidden">
+              <Image
+                src={data.mobileImage || ""}
+                alt={data.title}
+                width={300}
+                height={360}
+                className="w-[250px] h-auto object-cover shadow-md"
+                priority
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
